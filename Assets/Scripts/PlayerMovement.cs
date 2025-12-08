500287b8596d28d495391f656f5ed71cf77a8089
CHHJ0319 <83933117+CHHJ0319@users.noreply.github.com>
2025-12-08 13:14:07 +0900

    Bladez-003 : [Task] Modified PlayerMovement script based on the official UnityChan script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,42 +1,173 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed = 20f;
+    public float animSpeed = 1.5f;
 
-    Animator m_Animator;
-    Rigidbody m_Rigidbody;
-    Vector3 m_Movement;
-    Quaternion m_Rotation = Quaternion.identity;
+    public float forwardSpeed = 7.0f;
+    public float backwardSpeed = 2.0f;
+    public float rotateSpeed = 2.0f;
+    public float jumpPower = 3.0f;
+
+    public bool useCurves = true;
+    public float useCurvesHeight = 0.5f;
+
+    private Vector3 velocity;
+    private float orgColHight;
+    private Vector3 orgVectColCenter;
+
+    private Rigidbody rb;
+    private Animator anim;
+    private AnimatorStateInfo currentBaseState;
+    private CapsuleCollider col;
+
+    static int idleState = Animator.StringToHash("Base Layer.Idle");
+    static int locoState = Animator.StringToHash("Base Layer.Locomotion");
+    static int jumpState = Animator.StringToHash("Base Layer.Jump");
+    static int restState = Animator.StringToHash("Base Layer.Rest");
 
     void Start()
     {
-        m_Animator = GetComponent<Animator>();
-        m_Rigidbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+
+        col = GetComponent<CapsuleCollider>();
+        orgColHight = col.height;
+        orgVectColCenter = col.center;
     }
 
+    // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");              
+        float v = Input.GetAxis("Vertical");
 
-        m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize();
+        InitAnim(h, v);
 
-        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
-        bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool("IsWalking", isWalking);
+        currentBaseState = anim.GetCurrentAnimatorStateInfo(0); 
+     
+        rb.useGravity = true;
 
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation(desiredForward);
+        SetVelocity(v);
+        HandleJump();
+        ApplyMovement(h);
+        HandleStateSpecificLogic();
     }
 
-    void OnAnimatorMove()
+    void InitAnim(float h, float v)
     {
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
-        m_Rigidbody.MoveRotation(m_Rotation);
+        anim.SetFloat("Speed", v);
+        anim.SetFloat("Direction", h);
+        anim.speed = animSpeed;
+    }
+
+    void SetVelocity(float v)
+    {
+        velocity = new Vector3(0, 0, v);
+        velocity = transform.TransformDirection(velocity);
+        if (v > 0.1)
+        {
+            velocity *= forwardSpeed;
+        }
+        else if (v < -0.1)
+        {
+            velocity *= backwardSpeed;
+        }
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (currentBaseState.fullPathHash == locoState)
+            {
+
+                if (!anim.IsInTransition(0))
+                {
+                    rb.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+                    anim.SetBool("Jump", true);
+                }
+            }
+        }
+    }
+
+    void ApplyMovement(float h)
+    {
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+        transform.Rotate(0, h * rotateSpeed, 0);
+    }
+
+    void HandleStateSpecificLogic()
+    {
+        if (currentBaseState.fullPathHash == locoState)
+        {
+            if (useCurves)
+            {
+                resetCollider();
+            }
+        }
+
+        else if (currentBaseState.fullPathHash == jumpState)
+        {
+            //cameraObject.SendMessage("setCameraPositionJumpView");
+            if (!anim.IsInTransition(0))
+            {
+                if (useCurves)
+                {
+                    float jumpHeight = anim.GetFloat("JumpHeight");
+                    float gravityControl = anim.GetFloat("GravityControl");
+                    if (gravityControl > 0)
+                        rb.useGravity = false;
+
+                    Ray ray = new Ray(transform.position + Vector3.up, -Vector3.up);
+                    RaycastHit hitInfo = new RaycastHit();
+
+                    if (Physics.Raycast(ray, out hitInfo))
+                    {
+                        if (hitInfo.distance > useCurvesHeight)
+                        {
+                            col.height = orgColHight - jumpHeight;
+                            float adjCenterY = orgVectColCenter.y + jumpHeight;
+                            col.center = new Vector3(0, adjCenterY, 0);
+                        }
+                        else
+                        {
+
+                            resetCollider();
+                        }
+                    }
+                }
+                anim.SetBool("Jump", false);
+            }
+        }
+
+        else if (currentBaseState.fullPathHash == idleState)
+        {
+            if (useCurves)
+            {
+                resetCollider();
+            }
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetBool("Rest", true);
+            }
+        }
+
+        else if (currentBaseState.fullPathHash == restState)
+        {
+
+            if (!anim.IsInTransition(0))
+            {
+                anim.SetBool("Rest", false);
+            }
+        }
+    }
+
+    void resetCollider()
+    {
+        col.height = orgColHight;
+        col.center = orgVectColCenter;
     }
 }
